090ae93ce83fd7f7f4556e4c6ee09c062342d43e
Agott123 <100030807@mvla.net>
2023-11-16 09:48:13 -0800

    Player controls and dash draft 1

--- G3Game/Assets/Scripts/Movement.cs
+++ G3Game/Assets/Scripts/Movement.cs
@@ -5,38 +5,66 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public int movementSpeed;
-
+    public float dashSpeed;
+    public float movementSpeed;
+    public float dampeningFactor;
     public Animator weaponAnim;
+    private Rigidbody2D rb;
+    private float ogSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
+        ogSpeed = movementSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("dash_press");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveX = 0;
+        float moveY = 0;
         if (Input.GetKey("a"))
         {
-            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            moveX--;
         }
         if (Input.GetKey("d"))
         {
-            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+            moveX++;
         }
         if (Input.GetKey("w"))
         {
-            transform.position += Vector3.up * movementSpeed * Time.deltaTime;
+            moveY++;
         }
         if (Input.GetKey("s"))
         {
-            transform.position += Vector3.down * movementSpeed * Time.deltaTime;
+            moveY--;
         }
 
+        rb.velocity = (Vector2.right*moveX+Vector2.up*moveY).normalized*movementSpeed;
         if (Input.GetMouseButtonDown(0))
         {
             weaponAnim.SetTrigger("Attack");
+        }
+        
+        movementSpeed = ogSpeed + (movementSpeed - ogSpeed) * dampeningFactor;
+    }
+    
+    public IEnumerator dash_press(){
+
+        while (true)
+        {
+            yield return null;
+           
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("Dash");
+                movementSpeed = dashSpeed;
+                yield return new WaitForSeconds(5);
+            }
+
+            
         }
     }
 }
