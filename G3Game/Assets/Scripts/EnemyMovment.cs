2ee134eb4a5903e84ae788d34fe6fdf33c952a33
Agott123 <100030807@mvla.net>
2023-11-30 08:55:37 -0800

    Enemy prefab now has attack with collider working

--- G3Game/Assets/Scripts/EnemyMovment.cs
+++ G3Game/Assets/Scripts/EnemyMovment.cs
@@ -8,18 +8,20 @@ public class EnemyMovment : MonoBehaviour
     public float searchRange;
     public float attackRange;
     public float moveSpeed;
-
+    public GameObject swordCollider;
+    
     private Transform self;
     private Rigidbody2D rb;
     private Vector2 Movement;
     private Animator anim;
-    private SpriteRenderer sr;
+    //private SpriteRenderer sr;
     void Start()
     {
         self = transform;
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        //sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        swordCollider.SetActive(false);
     }
 
     void FixedUpdate()
@@ -39,17 +41,18 @@ public class EnemyMovment : MonoBehaviour
             // Flip image to look left or right
             if (Movement.x < 0.01f)
             {
-                sr.flipX = true;
+                self.rotation = Quaternion.Euler(Vector3.up * 180f);
             }
             else
             {
-                sr.flipX = false;
+                self.rotation = Quaternion.identity;
             }
             
             // If distance is close enough, swing
             if (Vector3.Distance(self.position, hit.transform.position) < attackRange)
             {
                 anim.SetTrigger("Attack");
+                swordCollider.SetActive(true);
             }
         }
         else
@@ -61,6 +64,11 @@ public class EnemyMovment : MonoBehaviour
     void MoveCharacter(Vector2 direction1)
     {
         rb.MovePosition((Vector2)self.position + (Time.fixedDeltaTime*moveSpeed*direction1));
+    }
+
+    public void DisableSword()
+    {
+        swordCollider.SetActive(false);
     }
 
 }
